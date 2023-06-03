266f9f68cac7c9090471386ab00d47b721daeead
Quinn <quinn@indra.ai>
2023-06-03 10:51:58 -0700

    update error deva to latest standards

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 // Copyright (c)2022 Quinn Michaels
 // The main Error Deva
-
 const fs = require('fs');
 const path = require('path');
-
 const package = require('./package.json');
 const info = {
   id: package.id,
@@ -17,73 +15,24 @@ const info = {
   license: package.license,
   copyright: package.copyright,
 };
-
 const data_path = path.join(__dirname, 'data.json');
 const {agent,vars} = require(data_path).DATA;
-
 const Deva = require('@indra.ai/deva');
 const ERROR = new Deva({
   info,
-  agent: {
-    id: agent.id,
-    key: agent.key,
-    prompt: agent.prompt,
-    profile: agent.profile,
-    translate(input) {
-      return input.trim();
-    },
-    parse(input) {
-      return input.trim();
-    }
-  },
+  agent,
   vars,
-  deva: {},
+  utils: {
+    translate(input) {return input.trim();},
+    parse(input) {return input.trim();},
+    process(input) {return input.trim();}
+  },
   listeners: {},
   modules: {},
   func: {
-    error(opts) {}
+    error(err) {return;}
   },
-  methods: {
-    /**************
-    method: uid
-    params: packet
-    describe: Return system unique id.
-    ***************/
-    uid(packet) {
-      this.context('uid');
-      return Promise.resolve(this.uid());
-    },
-
-    /**************
-    method: status
-    params: none
-    describe: Return the status of the Error Deva.
-    ***************/
-    status() {
-      this.context('status');
-      return Promise.resolve(this.status());
-    },
-
-    /**************
-    method: help
-    params: packet
-    describe: Return the Error Deva Help files.
-    ***************/
-    help(packet) {
-      this.context('help');
-      return new Promise((resolve, reject) => {
-        this.help(packet.q.text, __dirname).then(help => {
-          return this.question(`#feecting parse ${help}`);
-        }).then(parsed => {
-          return resolve({
-            text: parsed.a.text,
-            html: parsed.a.html,
-            data: parsed.a.data,
-          });
-        }).catch(reject);
-      });
-    },
-  },
+  methods: {},
   onDone(data) {
     this.listen('error', this.func.error);
   }
